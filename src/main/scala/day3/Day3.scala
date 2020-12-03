3946f3ec56f4e0d0a52e0982fa6aaaeab10a99a9
Ángel Miguel García Vico <agvico@ujaen.es>
2020-12-03 13:53:56 +0100

    Adds day 3 :)

--- src/main/scala/day3/Day3.scala
+++ src/main/scala/day3/Day3.scala
@@ -13,21 +13,23 @@ object Day3 {
       pos = (pos + 3) % s.length
       if(s.charAt(pos) == '#') 1 else 0
     }).sum
+
     println(trees)
 
     // part 2
     val positions: Array[(Int,Int)] = Array((1,1) , (3,1), (5,1) ,(7,1) ,(1,2))
 
     val solution = positions.map(x =>{
-      var downBy = 0
       pos = 0
-      data.filter(s => {downBy += 1; downBy % x._2 == 0})
+      data.zipWithIndex
+        .filter(i => (i._2 + 1) % x._2 == 0)
         .map(s => {
-        pos = (pos + x._1) % s.length
-        if (s.charAt(pos) == '#') 1 else 0
-      }).sum
+          pos = (pos + x._1) % s._1.length
+          if (s._1.charAt(pos) == '#') 1 else 0
+        }).sum
     }).product
 
     println(solution)
+
   }
 }
