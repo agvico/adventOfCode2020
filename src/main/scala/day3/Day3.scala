47c75c640dfef5dec572b2ecfb0c015214bda485
Ángel Miguel García Vico <agvico@ujaen.es>
2020-12-03 16:10:16 +0100

    Adds day 3 :)

--- src/main/scala/day3/Day3.scala
+++ src/main/scala/day3/Day3.scala
@@ -5,31 +5,34 @@ import scala.io.Source
 object Day3 {
   def main(args: Array[String]): Unit = {
 
-    val data = Source.fromFile("src/main/scala/day3/day3_data").getLines().toSeq.drop(1)     // ignore first line
-
-    // part1
-    var pos = 0
-    val trees = data.map(s => {
-      pos = (pos + 3) % s.length
-      if(s.charAt(pos) == '#') 1 else 0
+    // Part1
+    val trees = Source.fromFile("src/main/scala/day3/day3_data")
+      .getLines()
+      .toSeq
+      .zipWithIndex
+      .filter(_._2 != 0)
+      .map(s => {
+      if(s._1.charAt((s._2 * 3) % s._1.length) == '#') 1 else 0
     }).sum
-
     println(trees)
 
-    // part 2
-    val positions: Array[(Int,Int)] = Array((1,1) , (3,1), (5,1) ,(7,1) ,(1,2))
-
-    val solution = positions.map(x =>{
-      pos = 0
-      data.zipWithIndex
-        .filter(i => (i._2 + 1) % x._2 == 0)
+    // Part2
+    val sol = Array((3,1),(1,1),(5,1),(7,1),(1,2)).map(x => {
+      Source.fromFile("src/main/scala/day3/day3_data")
+        .getLines()
+        .toSeq
+        .zipWithIndex.filter(_._2 != 0)
+        .filter(_._2 % x._2 == 0)
         .map(s => {
-          pos = (pos + x._1) % s._1.length
+          val pos = ((s._2 / x._2) * x._1) % s._1.length
           if (s._1.charAt(pos) == '#') 1 else 0
         }).sum
+
     }).product
 
-    println(solution)
+    println(sol)
+
+
 
   }
 }
