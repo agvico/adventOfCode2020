a5295f84df66022c06c1a667d305cfea384fc60b
Ángel Miguel García Vico <agvico@ujaen.es>
2020-12-05 11:31:16 +0100

    Adds day 5 🦄

--- src/main/scala/day5/Day5.scala
+++ src/main/scala/day5/Day5.scala
@@ -8,11 +8,12 @@ object Day5 {
     val data = Source.fromFile("src/main/scala/day5/input").getLines()
 
     // Part1
-    val seatsIDs = data.map(line => {
-      val row: Int = Integer.parseInt(line.substring(0,7).map(char => if(char == 'F') '0' else '1' ), 2)
-      val column: Int = Integer.parseInt(line.substring(7,line.length).map(char => if(char == 'L') '0' else '1'), 2)
-      (row, column)
-    }).map(seat => seat._1 * 8 + seat._2).toSeq
+    val seatsIDs = data.map(toBinary)
+      .map(l => {
+        val row = Integer.parseInt(l.substring(0,7), 2)
+        val col = Integer.parseInt(l.substring(7,l.length), 2)
+        (row,col)
+      }).map(seat => seat._1 * 8 + seat._2).toSeq
 
     println(seatsIDs.max)
 
@@ -26,6 +27,12 @@ object Day5 {
       case None => - 1
     }
     println(myId)
+  }
+
+
+  def toBinary(s: String): String = {
+    val aux = "[FL]".r.replaceAllIn(s,"0")
+    "[BR]".r.replaceAllIn(aux,"1")
   }
 
 }
